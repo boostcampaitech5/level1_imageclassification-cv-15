0ae60fd8eed5926db0abcd9d88dc76d562205865
Kang Nahoon <ejrtks1020@naver.com>
2023-04-17 02:43:20 +0000

    edit config parsing

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ import torch
 from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
-
+from easydict import EasyDict
 from dataset import MaskBaseDataset
 from loss import create_criterion
 import wandb
@@ -73,6 +73,10 @@ def figure_to_array(fig):
     fig.canvas.draw()
     return np.array(fig.canvas.renderer._renderer)
 
+def read_json(fname):
+    fname = Path(fname)
+    with fname.open('rt') as handle:
+        return json.load(handle, object_hook=EasyDict)
 
 def increment_path(path, exist_ok=False):
     """ Automatically increment path, i.e. runs/exp --> runs/exp0, runs/exp1 etc.
@@ -276,31 +280,34 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     # Data and model checkpoints directories
-    parser.add_argument('--seed', type=int, default=42, help='random seed (default: 42)')
-    parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train (default: 1)')
-    parser.add_argument('--dataset', type=str, default='MaskBaseDataset', help='dataset augmentation type (default: MaskBaseDataset)')
-    parser.add_argument('--augmentation', type=str, default='BaseAugmentation', help='data augmentation type (default: BaseAugmentation)')
-    parser.add_argument("--resize", nargs="+", type=int, default=[128, 96], help='resize size for image when training')
-    parser.add_argument('--batch_size', type=int, default=64, help='input batch size for training (default: 64)')
-    parser.add_argument('--valid_batch_size', type=int, default=256, help='input batch size for validing (default: 256)')
-    parser.add_argument('--model', type=str, default='BaseModel', help='model type (default: BaseModel)')
-    parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer type (default: SGD)')
-    parser.add_argument('--lr', type=float, default=1e-3, help='learning rate (default: 1e-3)')
-    parser.add_argument('--val_ratio', type=float, default=0.2, help='ratio for validaton (default: 0.2)')
-    parser.add_argument('--criterion', type=str, default='f1', help='criterion type (default: f1)')
-    parser.add_argument('--lr_decay_step', type=int, default=20, help='learning rate scheduler deacy step (default: 20)')
-    parser.add_argument('--log_interval', type=int, default=20, help='how many batches to wait before logging training status')
-    parser.add_argument('--name', default='exp', help='model save at {SM_MODEL_DIR}/{name}')
-    parser.add_argument('--wandb', type=bool, default=True, help='use wandb')
+    parser.add_argument('-c', '--config', default='./config.json', type=str, help='config file path (default: ./config.json)')
+    # parser.add_argument('--seed', type=int, default=42, help='random seed (default: 42)')
+    # parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train (default: 1)')
+    # parser.add_argument('--dataset', type=str, default='MaskBaseDataset', help='dataset augmentation type (default: MaskBaseDataset)')
+    # parser.add_argument('--augmentation', type=str, default='BaseAugmentation', help='data augmentation type (default: BaseAugmentation)')
+    # parser.add_argument("--resize", nargs="+", type=int, default=[128, 96], help='resize size for image when training')
+    # parser.add_argument('--batch_size', type=int, default=64, help='input batch size for training (default: 64)')
+    # parser.add_argument('--valid_batch_size', type=int, default=256, help='input batch size for validing (default: 256)')
+    # parser.add_argument('--model', type=str, default='BaseModel', help='model type (default: BaseModel)')
+    # parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer type (default: SGD)')
+    # parser.add_argument('--lr', type=float, default=1e-3, help='learning rate (default: 1e-3)')
+    # parser.add_argument('--val_ratio', type=float, default=0.2, help='ratio for validaton (default: 0.2)')
+    # parser.add_argument('--criterion', type=str, default='f1', help='criterion type (default: f1)')
+    # parser.add_argument('--lr_decay_step', type=int, default=20, help='learning rate scheduler deacy step (default: 20)')
+    # parser.add_argument('--log_interval', type=int, default=20, help='how many batches to wait before logging training status')
+    # parser.add_argument('--name', default='exp', help='model save at {SM_MODEL_DIR}/{name}')
+    # parser.add_argument('--wandb', type=bool, default=True, help='use wandb')
 
-    # Container environment
-    parser.add_argument('--data_dir', type=str, default=os.environ.get('SM_CHANNEL_TRAIN', '/opt/ml/input/data/train/images'))
-    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_MODEL_DIR', './model'))
+    # # Container environment
+    # parser.add_argument('--data_dir', type=str, default=os.environ.get('SM_CHANNEL_TRAIN', '/opt/ml/input/data/train/images'))
+    # parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_MODEL_DIR', './model'))
 
     args = parser.parse_args()
-    print(args)
+    # print(args)
+    config = read_json(args.config)
+    print(config)
 
-    data_dir = args.data_dir
-    model_dir = args.model_dir
+    data_dir = config.data_dir
+    model_dir = config.model_dir
 
-    train(data_dir, model_dir, args)
+    train(data_dir, model_dir, config)
